e3e10313e4df0eba20ac360c2b972f192358005f
ImranYounas123 <imran.khanfaa123@gmail.com>
2022-05-03 00:37:00 -0700

    User routes done through API

--- backend/Controller/Order.js
+++ backend/Controller/Order.js
@@ -19,10 +19,33 @@ exports.CreateOrder = CatchAsynErros(async(req,res,next)=>{
     })
 });
 
-// Get single Order
+//  Get single Order
 exports.getSingleOrder = CatchAsynErros(async (req,res,next)=>{
-    
-})
+    // as we used UserID in order DB then by populate method => will go UserDb and get email or pwd 
+    const order = await Order.findById(req.params.id).populate("user","name email");
+
+    if(!order){
+        return next (new ErrorHander(`Order not found With This ID ${req.params.id}`, 404))
+    }
+    res.status(200).json({
+        succuss : true,
+        order,
+    });
+});
+
+// (LogedIn) Get single Order
+exports.getmyOrders = CatchAsynErros(async (req,res,next)=>{
+// we find Orders have in user field id ,_id => orders , User=> who ordered
+    const order = await Order.find({user : req.user._id});
+
+    if(!order){
+        return next (new ErrorHander(`Order not found With This ID ${req.params.id}`, 404))
+    }
+    res.status(200).json({
+        succuss : true,
+        order,
+    });
+});
 
 
 
