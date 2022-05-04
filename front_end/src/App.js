c1eff89463134d17490066596175781b4a1a5bfb
ImranYounas123 <imran.khanfaa123@gmail.com>
2022-05-04 07:31:48 -0700

    Header footer UI Completed

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import './app.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from "./Components/layout/Header/Header"
+import Footer from "./Components/layout/footer/Footer";
+import Home from "./Components/Home/Home.js"
 function App() {
   return (
-    <div className="App">
-      <h1>App Comp </h1>
-    </div>
+    <Router>
+    <Header />
+    <Route exact path="/" component={Home}/>
+    <Footer />
+  </Router>
   );
 }
-
 export default App;
